ff27bf7977b53974fc2d021c65311804a00c65f2
Fuchs7 <maksim.p.nik@gmail.ru>
2014-04-27 19:12:00 +0700

    add AutoGenField.cs

---
+++ seaBattle/Common/AutoGenerationFild.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seaBattle.Common
+{
+
+    public class AutoGenerationFild
+    {
+
+        public bool[,] GetFilledIn()
+        {
+            throw new Exception();
+        }
+    }
+}
