f287a58f607df4e1f1f3510b9b790526cadddb5f
3la9lcObAkA <3la9lcobAkA@gmail.com>
2014-04-28 21:12:22 +0700

    Add Generation

--- seaBattle/Common/AutoGenerationFild.cs
+++ seaBattle/Common/AutoGenerationFild.cs
@@ -9,10 +9,126 @@ namespace seaBattle.Common
     
     public class AutoGenerationFild
     {
+        private bool[,] BattleField = new bool[10, 10];
+        private bool CanFilledIn(bool x,int min,int max, int pos)
+        {
+            int Error=0;
+            if ((min>=0) && ( max <=9) && (max>=min) && (pos>=0) && (pos<=9))
+            {
+            int minYAvail, minXAvail, maxYAvail, maxXAvail;
+            if (x) 
+            {
+                if (min > 0) { minXAvail = min - 1; }
+                else { minXAvail = min; };
+                if (max < 9) { maxXAvail = max + 1; }
+                else { maxXAvail = max; };
+                if (pos > 0) { minYAvail = pos - 1; }
+                else { minYAvail = pos;};
+                if (pos < 9) { maxYAvail = pos + 1; }
+                else { maxYAvail = pos; };
+            }
+            else
+            {
+                if (min > 0) { minYAvail = min - 1; }
+                else { minYAvail = min; };
+                if (max < 9) { maxYAvail = max + 1; }
+                else { maxYAvail = max; };
+                if (pos > 0) { minXAvail = pos - 1; }
+                else { minXAvail = pos;};
+                if (pos < 9) { maxXAvail = pos + 1; }
+                else { maxXAvail = pos;};
+            };
 
+
+            for (int i = minXAvail;i<=maxXAvail;i++)
+            {
+            for (int j = minYAvail;j<=maxYAvail;j++)
+            {
+               if (x)
+               { 
+                   if (BattleField[i,j]) { Error = 1; }
+               }
+               else
+               {
+                   if (BattleField[j, i]) { Error = 1; }
+               };
+            };
+            };
+            if (Error == 0) { return true; }
+            else { return false; };
+            }
+            else {return false;};
+        }
         public bool[,] GetFilledIn()
         {
-            throw new Exception();
+            int x,y;
+            int navi;
+            int length = 4;
+            bool filled = false;
+            int currentcount=0;
+            int count = 1;
+            bool end = false ;
+            Random rand = new Random();
+            do
+            {
+            x = rand.Next(10);
+            y = rand.Next(10);            
+            navi = rand.Next(1, 4);
+            switch (navi)
+            {
+                case 1:
+                    {
+                        if (CanFilledIn(true,x - (length-1),x,y))
+                        {
+                            for (int i = x - (length - 1); i <= x; i++)
+                                BattleField[i, y] = true;
+                            filled = true;
+                        }
+                        break;
+                    };
+                case 2:
+                    {
+                        if (CanFilledIn(false,y - (length-1),y,x))
+                        {
+                            for (int i = y - (length - 1); i <= y; i++)
+                                BattleField[x, i] = true;
+                            filled = true;
+                        }
+                        break;
+                    };
+                case 3:
+                    {
+                        if (CanFilledIn(true, x, x + (length - 1), y))
+                        {
+                            for (int i = x + (length - 1); i >= x; i--)
+                                BattleField[i, y] = true;
+                            filled = true;
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        if (CanFilledIn(false, y, y + (length - 1), x))
+                        {
+                            for (int i = y + (length - 1); i >= y; i--)
+                                BattleField[x, i] = true;
+                            filled = true;
+                        }
+                        break;
+                    };
+            };
+            if (filled) { currentcount++; filled = false; };
+            if (currentcount==count)
+            {
+                currentcount = 0;
+                count++;
+                length--;
+            };
+            if (count == 5) { end = true; }
+            }while (!end);
+            return BattleField;
+            
+
         }
     }
 }
